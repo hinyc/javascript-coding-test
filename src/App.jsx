2dfe7a9533dd56c7015ef408c0a00c440b45c67f
Inyeol.hong <inyeol.hong@2bytescorp.com>
2024-04-30 16:54:51 +0900

    start

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import ValidAnagram from './page/validAnagram';
+
+function App() {
+  return (
+    <>
+      <div>ArrayDeduplication</div>
+      {/* <ArrayDeduplication /> */}
+      <ValidAnagram />
+    </>
+  );
+}
+
+export default App;
---
+++ src/page/validAnagram/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function ValidAnagram() {
+  //valid anagram
+  //두 단어의 글자 나열 순서는 다르지만 구성이 일치할시 -> anagram
+  //split, sort, join
+  //hashmap
+
+  const stringA = 'listen';
+  const stringB = 'silent';
+
+  return <div>ValidAnagram</div>;
+}
